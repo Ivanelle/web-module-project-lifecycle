552db41e2b1f75a6dbe8ffe4fabbffdad5966e2e
Ivanelle Hunt <ivanellep@gmail.com>
2023-10-03 17:00:28 -0700

    todo list rendering

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    }
+  }
+  fetchTodos = () => {
+    axios.get(URL)
+    .then(res => {
+      this.setState({ ...this.state, todos: res.data.data })
+    })
+    .catch(err => {
+      this.setState({ ...this.state, error: err.res.data.message })
+    })
+  }
+  componentDidMount() {
+    this.fetchTodos()
+  }
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>Error: {this.state.error}</div>
+        <h2>Todos:</h2>
+        {
+          this.state.todos.map(td => {
+            return <div key={td.id} >{td.name}</div>
+          })
+        }
+      </div>
+    )
   }
 }
