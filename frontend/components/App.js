dbc3c49836187cfb6216e0d89108207e14c66e5a
Ivanelle Hunt <ivanellep@gmail.com>
2023-10-03 18:12:31 -0700

    patch works

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,7 +8,6 @@ export default class App extends React.Component {
     super();
     this.state = {
       todos: [],
-      error: '',
       todoInput: ''
     } 
     }
@@ -21,11 +20,11 @@ export default class App extends React.Component {
   newTodo = () => {
     axios.post(URL, { name: this.state.todoInput})
     .then (res => {
-    this.fetchTodos()
+    this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
     this.setState({ ...this.state, todoInput: ''})
     })
     .catch(err => {
-      this.setState({ ...this.state, error: err.res.data.message })
+      this.setState({ ...this.state, error: err.response.data.message })
     })
   }
 
@@ -40,23 +39,33 @@ export default class App extends React.Component {
       this.setState({ ...this.state, todos: res.data.data })
     })
     .catch(err => {
-      this.setState({ ...this.state, error: err.res.data.message })
+      this.setState({ ...this.state, error: err.response.data.message })
     })
   }
 
-
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+    .then(res => {
+      this.setState({ ...this.state, todos: this.state.todos.map(td => {
+        if (td.id !== id) return td
+        return res.data.data
+      })})
+    })
+    .catch(err => {
+      this.setState({ ...this.state, error: err.response.data.message })
+    })
+  }
   componentDidMount() {
     this.fetchTodos()
   }
   render() {
     return (
       <div>
-        <div id='error'>Error: {this.state.error}</div>
         <div id='todos'>
           <h2>Todos:</h2>
           {
             this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
+              return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name}{td.completed ? ' 👍' : ''}</div>
             })
           }
         </div>
